3ec013f25fa5aa07cf3430827819b2bb7ae8c9a8
Rumyana Kaludeva <rumyana.kaludeva@gmail.com>
2026-04-03 12:20:18 +0300

    feat: register courage story template

--- lib/story/templateRegistry.ts
+++ lib/story/templateRegistry.ts
@@ -1,5 +1,7 @@
 import { girlKindnessTemplate } from "@/app/templates/girl-kindness";
+import { courageTemplate } from "@/app/templates/courage";
 
 export const storyTemplates = {
   kindness: girlKindnessTemplate,
+  courage: courageTemplate,
 };
